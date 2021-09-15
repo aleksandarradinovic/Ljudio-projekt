8e2b379487715759d91e02606d2f55f8f5d59167
aleksandarradinovic <Alekss.radinovic@hotmail.com>
2021-09-15 18:51:21 +0200

    Skapat en Header component och en kort global styling samt stylat headern

--- vue-spotify/src/App.vue
+++ vue-spotify/src/App.vue
@@ -1,12 +1,29 @@
 <template>
-  
+  <Header />
 </template>
 
 <script>
 
+import Header from './components/Header.vue'
+
+export default{
+    components: {
+        Header,
+    }
+}
 
 </script>
 
 <style>
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body{
+    font-family: sans-serif;
+}
 
 </style>
---
+++ vue-spotify/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <h2>Spotif</h2>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'header',
+}
+</script>
+
+<style scoped>
+
+header{
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    padding: 15px;
+    background-color: #212121;
+    color: rgb(46, 173, 7);
+
+}
+
+</style>
