49838573ab7c5bb437cbada441875004aefec69b
aleksandarradinovic <Alekss.radinovic@hotmail.com>
2021-09-18 11:53:03 +0200

    added bootstrap 4 to my project, and added some more style

--- vue-spotify/src/App.vue
+++ vue-spotify/src/App.vue
@@ -1,5 +1,16 @@
-<template>
-  <Header />
+<template> 
+    <body>
+        <Header/>
+        <div class="container">
+            <form>
+                <label for="inputText" class="form-label">Search</label>
+                <input type="text" id="inputText5" class="form-control">
+                <button type="button" class="btn btn-secondary">Search</button>
+            </form>
+        </div >
+        <div class="container2">
+        </div>
+    </body>
 </template>
 
 <script>
@@ -7,6 +18,7 @@
 import Header from './components/Header.vue'
 
 export default{
+    name: 'App',
     components: {
         Header,
     }
@@ -24,6 +36,32 @@ export default{
 
 body{
     font-family: sans-serif;
+    align-content: center;
+  align-items: center;
+}
+
+.container {
+  width: 800px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 100px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+  background-color: white;
+}
+
+.container2 {
+  width: 1000px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 100px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+  background-color: white;
+  align-content: center;
+  align-items: center;
 }
 
 </style>
